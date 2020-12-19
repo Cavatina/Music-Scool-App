7c401597efe30bf1addd7ea9c2f49dbc0eb48ff6
Kristian Berge Nessa <kristian@cavatina.no>
2020-12-19 14:10:25 +0100

    Remove package:shared_preferences

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -18,7 +18,6 @@ import 'package:flutter/material.dart';
 import 'package:musicscool/services/api.dart';
 import 'package:musicscool/viewmodels/auth.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'login_page.dart';
 import 'home_page.dart';
@@ -34,7 +33,6 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   Widget _waitingPage() {
     return Scaffold(
@@ -49,8 +47,6 @@ class _RootPageState extends State<RootPage> {
   void initState() {
     initializeDateFormatting().then((obj){});
     super.initState();
-    _prefs.then((SharedPreferences prefs) {
-    });
   }
 
   @override
